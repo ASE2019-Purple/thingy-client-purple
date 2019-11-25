cd735c121bd1db6c1391b5fe75c8fc7398a3dae8
brns <brnsnt@protonmail.ch>
2019-11-25 23:27:28 +0100

    TG-24 use public path in redirection callback

--- src/auth/index.js
+++ src/auth/index.js
@@ -1,11 +1,11 @@
 import Vue from "vue";
 import createAuth0Client from "@auth0/auth0-spa-js";
 
-let public_path = process.env.PUBLIC_PATH;
+let public_path = process.env.PUBLIC_PATH || window.location.pathname;
 
 /** Define a default action to perform after authentication */
 const DEFAULT_REDIRECT_CALLBACK = () =>
-  window.history.replaceState({}, document.title, public_path  || window.location.pathname);
+  window.history.replaceState({}, document.title, public_path );
 
 let instance;
 
@@ -15,7 +15,7 @@ export const getInstance = () => instance;
 /** Creates an instance of the Auth0 SDK. If one has already been created, it returns that instance */
 export const useAuth0 = ({
   onRedirectCallback = DEFAULT_REDIRECT_CALLBACK,
-  redirectUri = window.location.origin,
+  redirectUri = public_path,
   ...options
 }) => {
   if (instance) return instance;
